7e9d19aea725a4a3128fab07d14da01c7ea3ba5e
Pablo Martin Morente <politotuto@gmail.com>
2025-10-08 18:20:40 +0200

    returns serverSize, clientVersion and managerVersion from programData

--- scripts/serverCharts.py
+++ scripts/serverCharts.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import json
-import sys
+import pickle
 current_directory = os.getcwd()
 total, used, free = shutil.disk_usage(current_directory)
 
@@ -13,12 +13,29 @@ def get_directory_size(directory):
             if os.path.exists(file_path):
                 total_size += os.path.getsize(file_path)
     return total_size
+
+
+def readData(index):
+    info=None 
+    program_data = os.environ.get("PROGRAMDATA", r"C:\ProgramData")
+    app_data_dir = os.path.join(program_data, "TizonaHub")
+    data_file = os.path.join(app_data_dir, "data.dat")
+    try:
+        with open(data_file, "rb") as f:
+                info = pickle.load(f)
+                return info if not index else list(info.values())[index]
+    except Exception as e:
+        print('Error at readData',e)
+        return False
     
 result={ 
     "total":round(total),
     "used":round(used),
     "free":round(free),
-    "serverSize":round(get_directory_size(current_directory))
+    "serverSize":round(get_directory_size(current_directory)),
+    "serverVersion":readData(2),
+    "clientVersion":readData(1),
+    "managerVersion":readData(3)
 }
 
 
